74e655e42d7d0e7066ecee8dd22cd10071f47ac0
surtweig <surtweig@gmail.com>
2014-01-13 03:16:49 +0400

    added proto-textmode

---
+++ proto-textmode/proto_textmode.dpr
@@ -0,0 +1,14 @@
+program proto_textmode;
+
+uses
+  Forms,
+  FMain in 'FMain.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
