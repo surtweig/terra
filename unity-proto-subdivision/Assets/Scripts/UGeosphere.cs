7fcbd495b6e6a7893127358e33f72feeef819c50
surtweig <surtweig@gmail.com>
2014-03-16 22:54:05 +0400

    unity-proto-subdivision: геосфера

---
+++ unity-proto-subdivision/Assets/Scripts/UGeosphere.cs
@@ -0,0 +1,368 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TGeoNode
+{
+	public Vector3 position;
+	public Vector3 normal;
+	public float radius;
+	public List<int[]> adjacency;
+	
+	public TGeoNode()
+	{
+		adjacency = new List<int[]>();
+	}
+}
+
+public class TGeoTriTreeNode
+{
+	public int[] vertices;
+	public int[] children;
+}
+
+public class TMutableGeoMesh
+{
+	public List<int> indexes;
+	public List<int> triangles;
+	
+	public TMutableGeoMesh()
+	{
+		indexes = new List<int>();
+		triangles = new List<int>();
+	}
+	
+	public void AddVertex(int nodeIndex)
+	{
+		if (indexes.Count == 0)
+			indexes.Add(nodeIndex);
+		else
+		{
+			int subIndex = indexes.BinarySearch(nodeIndex);
+			if (subIndex < 0)
+			{
+				subIndex = ~subIndex;
+				indexes.Insert(subIndex, nodeIndex);
+			}
+			
+		}
+		triangles.Add(nodeIndex);
+	}
+	
+	public void IndexTriangles()
+	{
+		for (int i = 0; i < triangles.Count; i++)
+			triangles[i] = indexes.BinarySearch(triangles[i]);
+	}
+}
+
+public class TGeosphere {
+	
+	// -------------------------------------------------------
+	public TGeosphere(int seed = 0)
+	{
+		xSeed = seed;
+		xPerlinNoise = new TPerlin3DNoise(xSeed);
+	}
+	
+	public void Clear()
+	{
+		xNodes.Clear();
+		xTriTree.Clear();
+		xSubdivisionLevel = 0;
+	}
+
+	public void BuildIcosahedron()
+	{
+		const float A = 0.5f;
+		const float B = 0.30901699437f; // 1/(1+Sqrt(5))
+		
+		Vector3[] icoverts = new Vector3[12]
+			{ new Vector3(0f, -B, -A), new Vector3(0f, -B, A), new Vector3(0f, B, -A), new Vector3(0f, B, A),
+			  new Vector3(-A, 0f, -B), new Vector3(-A, 0f, B), new Vector3(A, 0f, -B), new Vector3(A, 0f, B),
+			  new Vector3(-B, -A, 0f), new Vector3(-B, A, 0f), new Vector3(B, -A, 0f), new Vector3(B, A, 0f) };
+			
+		int[][] icotris = new int[20][]
+			{ new int[3] {2, 9, 11}, new int[3] {3, 11, 9}, new int[3] {3, 5, 1},  new int[3] {3, 1, 7},  new int[3] {2, 6, 0},
+			  new int[3] {2, 0, 4},  new int[3] {1, 8, 10}, new int[3] {0, 10, 8}, new int[3] {9, 4, 5},  new int[3] {8, 5, 4},
+			  new int[3] {11, 7, 6}, new int[3] {10, 6, 7}, new int[3] {3, 9, 5},  new int[3] {3, 7, 11}, new int[3] {2, 4, 9},
+			  new int[3] {2, 11, 6}, new int[3] {0, 8, 4},  new int[3] {0, 6, 10}, new int[3] {1, 5, 8},  new int[3] {1, 10, 7} };
+		
+		int[][] icoadj = new int[12][]
+			{ new int[5] {8, 2, 4, 10, 6}, new int[5] {8, 10, 3, 5, 7}, new int[5] {0, 9, 11, 4, 6},  new int[5] {7, 9, 11, 5, 1},
+			  new int[5] {0, 9, 2, 5, 8},  new int[5] {8, 1, 3, 4, 9},  new int[5] {0, 2, 11, 10, 7}, new int[5] {11, 1, 10, 3, 6},
+			  new int[5] {0, 1, 10, 4, 5}, new int[5] {3, 2, 11, 4, 5}, new int[5] {8, 1, 6, 0, 7},   new int[5] {9, 2, 3, 6, 7} };		
+		
+		for (int i = 0; i < icoverts.Length; i++)
+		{
+			TGeoNode node = new TGeoNode();
+			node.position = icoverts[i].normalized;
+			node.radius = 1f;
+			node.adjacency.Add(icoadj[i]);
+			xNodes.Add(node);
+		}
+		
+		for (int i = 0; i < icotris.Length; i++)
+			xAddTriTreeNode(icotris[i]);
+	}
+
+	public void Subdivide(int depth = 1)
+	{
+		
+		for (int iter = 0; iter < depth; iter++)
+		{
+			for (int i = 0; i < IcosahedronTriangles; i++)
+				xSubdivide(i);
+			
+			xSubdivisionLevel++;
+		}
+	}
+	
+	public Mesh GenerateMesh(int depth = -1, int region = -1, bool generateUV = false)
+	{
+		TMutableGeoMesh geomesh = new TMutableGeoMesh();
+		xCollectVertexes(geomesh, region, depth);
+		geomesh.IndexTriangles();
+		
+		BuildNormals();
+		
+		List<Vector3> vertices = new List<Vector3>();
+		List<Vector3> normals = new List<Vector3>();
+		List<Vector2> uv = new List<Vector2>();
+		
+		foreach (int nodeIndex in geomesh.indexes)
+		{
+			vertices.Add(xNodes[nodeIndex].position);
+			normals.Add(xNodes[nodeIndex].normal);
+		}
+		
+		Debug.Log(vertices.Count);
+		Debug.Log(geomesh.triangles.Count);
+
+		Mesh mesh = new Mesh();
+		mesh.vertices = vertices.ToArray();
+		mesh.normals = normals.ToArray();
+		mesh.triangles = geomesh.triangles.ToArray();
+		return mesh;
+	}
+	
+	public void BuildNormals()
+	{
+		for (int i = 0; i < xNodes.Count; i++)
+		{
+			float nx = 0f;
+			float ny = 0f;
+			float nz = 0f;
+			
+			int highlev = xNodes[i].adjacency.Count-1;
+			
+			int icurrent = -1;
+			Vector3 vcurrent = xNodes[i].position;
+
+			int inext = xNodes[i].adjacency[highlev][0];
+			Vector3 vnext = xNodes[inext].position;
+			
+			int ifirst = inext;
+			
+			while (true)
+			{
+				nx += (vcurrent.y - vnext.y) * (vcurrent.z + vnext.z);
+				ny += (vcurrent.z - vnext.z) * (vcurrent.x + vnext.x);
+				nz += (vcurrent.x - vnext.x) * (vcurrent.y + vnext.y);
+				
+				// finding next node as common adjacent of center and current node, but not previous
+				bool nextfound = false;
+				for (int j = 0; j < xNodes[i].adjacency[highlev].Length; j++)
+				{
+					for (int k = 0; k < xNodes[inext].adjacency[highlev].Length; k++)
+						if (xNodes[i].adjacency[highlev][j] == xNodes[inext].adjacency[highlev][k])
+							if (xNodes[i].adjacency[highlev][j] != icurrent)
+							{
+								icurrent = inext;
+								inext = xNodes[i].adjacency[highlev][j];
+								nextfound = true;
+								break;
+							}
+					
+					if (nextfound)
+						break;
+				}
+				
+				vcurrent = vnext;
+				vnext = xNodes[inext].position;
+				
+				if (inext == ifirst)
+					break;
+			}
+			
+			vnext = xNodes[i].position;
+			nx += (vcurrent.y - vnext.y) * (vcurrent.z + vnext.z);
+			ny += (vcurrent.z - vnext.z) * (vcurrent.x + vnext.x);
+			nz += (vcurrent.x - vnext.x) * (vcurrent.y + vnext.y);
+			
+			Vector3 normal = new Vector3(nx, ny, nz).normalized;
+			
+			if (Vector3.Dot(normal, vnext.normalized) < 0)
+				normal = -normal;
+			
+			xNodes[i].normal = normal;
+		}
+	}
+
+	protected void xCollectVertexes(TMutableGeoMesh mesh, int baseTriangleNode = -1, int depth = -1)
+	{
+		if (baseTriangleNode < 0)
+		{
+			for (int i = 0; i < IcosahedronTriangles; i++)
+				xCollectVertexes(mesh, i, depth);
+		}
+		else
+		{
+			if (depth == 0 || xTriTree[baseTriangleNode].children[0] < 0)
+			{
+				for (int i = 0; i < 3; i++)
+					mesh.AddVertex(xTriTree[baseTriangleNode].vertices[i]);
+			}
+			else
+			{
+				for (int i = 0; i < 4; i++)
+					xCollectVertexes(mesh, xTriTree[baseTriangleNode].children[i], depth-1);
+			}
+		}
+	}
+	
+	protected void xSubdivide(int treeNodeIndex)
+	{
+		if (xTriTree[treeNodeIndex].children[0] < 0)
+		{
+			int[] subNodes = new int[3];
+			
+			for (int i = 0; i < 3; i++)
+			{
+				int v1 = xTriTree[treeNodeIndex].vertices[i];
+				int v2 = xTriTree[treeNodeIndex].vertices[(i+1) % 3];
+				
+				int v12 = xFindNodesCommonAdjacency(v1, v2, xSubdivisionLevel+1);
+				
+				if (v12 < 0)
+				{
+					TGeoNode newnode = new TGeoNode();
+					newnode.position = (xNodes[v1].position + xNodes[v2].position).normalized;
+					newnode.radius = 1f;
+					newnode.adjacency = new List<int[]>();
+					v12 = xAddNode(newnode);
+					
+					xConnectNodes(v12, v1, xSubdivisionLevel+1);
+					xConnectNodes(v12, v2, xSubdivisionLevel+1);
+				}
+				
+				subNodes[i] = v12;
+			}
+			
+			xConnectNodes(subNodes[0], subNodes[1], xSubdivisionLevel+1);
+			xConnectNodes(subNodes[1], subNodes[2], xSubdivisionLevel+1);
+			xConnectNodes(subNodes[2], subNodes[0], xSubdivisionLevel+1);
+			
+			//     /\
+			//    /  \
+			//   /----\
+			//  / \  / \
+			// /___\/___\
+			
+			xAddTriTreeNode( new int[3] { xTriTree[treeNodeIndex].vertices[0], subNodes[0], subNodes[2] }, treeNodeIndex );
+			xAddTriTreeNode( new int[3] { xTriTree[treeNodeIndex].vertices[1], subNodes[1], subNodes[0] }, treeNodeIndex );
+			xAddTriTreeNode( new int[3] { xTriTree[treeNodeIndex].vertices[2], subNodes[2], subNodes[1] }, treeNodeIndex );
+			xAddTriTreeNode( new int[3] { subNodes[0], subNodes[1], subNodes[2] }, treeNodeIndex );
+		}
+		else
+			foreach (int childNode in xTriTree[treeNodeIndex].children)
+				xSubdivide(childNode);
+	}
+	
+	// -------------------------------------------------------
+	public const int IcosahedronTriangles = 20;
+	
+	public static int[,] IcosahedronTrianglePairs = new int[IcosahedronTriangles/2, 2]
+		{ {0, 1}, {2, 3}, {15, 4}, {14, 5}, {19, 6}, {16, 7}, {12, 8}, {18, 9}, {13, 10}, {17, 11} };
+	
+	public static int[] IcosahedronTriangleToPairsMap = new int[IcosahedronTriangles]
+		{ 0, 0, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 6, 8, 3, 2, 5, 9, 7, 4 };
+	
+	// -------------------------------------------------------
+	private List<TGeoNode> xNodes = new List<TGeoNode>();
+	private List<TGeoTriTreeNode> xTriTree = new List<TGeoTriTreeNode>();
+	private int xSubdivisionLevel = 0;
+	private int xSeed;
+	private TPerlin3DNoise xPerlinNoise;
+	
+	private int xAddNode(TGeoNode node)
+	{
+		int i = xNodes.Count;
+		xNodes.Add(node);
+		return i;
+	}
+	
+	private int xAddTriTreeNode(int[] vertices, int parentNode = -1)
+	{
+		TGeoTriTreeNode newNode = new TGeoTriTreeNode();
+		newNode.vertices = vertices;
+		newNode.children = new int[4] { -1, -1, -1, -1 };
+		int newNodeIndex = xTriTree.Count;
+		xTriTree.Add(newNode);
+		if (parentNode >= 0)
+			for (int i = 0; i < 4; i++)
+				if (xTriTree[parentNode].children[i] == -1)
+				{
+					xTriTree[parentNode].children[i] = newNodeIndex;
+					break;
+				}
+		return newNodeIndex;
+	}
+	
+	private void xAddNodeAdjacency(int nodeIndex, int level, int adjNodeIndex)
+	{
+		int maxLevel = xNodes[nodeIndex].adjacency.Count-1;
+		if (maxLevel < level)
+			for (int i = maxLevel; i < level; i++)
+				xNodes[nodeIndex].adjacency.Add(new int[] {});
+		
+		int[] adj = new int[xNodes[nodeIndex].adjacency[level].Length+1];
+		xNodes[nodeIndex].adjacency[level].CopyTo(adj, 0);
+		adj[adj.Length-1] = adjNodeIndex;
+		xNodes[nodeIndex].adjacency[level] = adj;
+	}
+	
+	private void xConnectNodes(int node1, int node2, int level)
+	{
+		xAddNodeAdjacency(node1, level, node2);
+		xAddNodeAdjacency(node2, level, node1);
+	}
+	
+	private int xFindNodesCommonAdjacency(int node1, int node2, int level)
+	{
+		if (xNodes[node1].adjacency.Count <= level || xNodes[node2].adjacency.Count <= level)
+			return -1;
+		foreach (int adj1 in xNodes[node1].adjacency[level])
+			foreach (int adj2 in xNodes[node2].adjacency[level])
+				if (adj1 == adj2)
+					return adj1;
+		return -1;
+	}
+	
+}
+
+public class UGeosphere : MonoBehaviour {
+	
+	private TGeosphere geo;
+	
+	void Start () {
+		geo = new TGeosphere();
+		geo.BuildIcosahedron();
+		geo.Subdivide(6);
+		GetComponent<MeshFilter>().mesh = geo.GenerateMesh();
+	}
+	
+	void Update () {
+	
+	}
+}
---
+++ unity-proto-subdivision/Assets/Standard Assets/Scripts/PerlinNoise.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Ported from GLScene project (http://glscene.org)
+
+// Generates Perlin Noise in the [-1; 1] range. 2D noise requests are taken in the Z=0 slice
+public class TPerlin3DNoise
+{
+	public const int cPERLIN_TABLE_SIZE = 256; // must be a power of two
+	private const int cMask = cPERLIN_TABLE_SIZE - 1;
+	
+	private int[] FPermutations = new int[cPERLIN_TABLE_SIZE];
+	private float[] FGradients = new float[cPERLIN_TABLE_SIZE*3];
+	
+	public TPerlin3DNoise(int randomSeed)
+	{
+		Initialize(randomSeed);
+	}
+	
+	public void Initialize(int randomSeed)
+	{
+		int seedBackup = Random.seed;
+		Random.seed = randomSeed;
+		
+		// Generate random gradient vectors
+		for (int i = 0; i < cPERLIN_TABLE_SIZE; i++)
+		{
+			float z = 1f - 2f * Random.value;
+			float r = Mathf.Sqrt(1f - z*z);
+			float alpha = 2f*Mathf.PI * Random.value;
+			FGradients[i*3] = r * Mathf.Sin(alpha);
+			FGradients[i*3+1] = r * Mathf.Cos(alpha);
+			FGradients[i*3+2] = z;
+		}
+		
+		// Initialize permutations table
+		for (int i = 0; i < cPERLIN_TABLE_SIZE; i++)
+			FPermutations[i] = i;
+		
+		// Shake up
+		for (int i = 0; i < cPERLIN_TABLE_SIZE; i++)
+		{
+			int j = Random.Range(0, cPERLIN_TABLE_SIZE);
+			int t = FPermutations[i];
+			FPermutations[i] = FPermutations[j];
+			FPermutations[j] = t;
+		}
+		
+		Random.seed = seedBackup;
+	}
+	
+	public float Noise(Vector2 v)
+	{
+		int ix = Mathf.FloorToInt(v.x);
+		float fx0 = v.x - ix;
+		float fx1 = fx0 - 1f;
+		float wx = Smooth(fx0);
+
+		int iy = Mathf.FloorToInt(v.y);
+		float fy0 = v.y - iy;
+		float fy1 = fy0 - 1f;
+		float wy = Smooth(fy0);
+		
+		float vy0 = Mathf.Lerp(Lattice(ix, iy, fx0, fy0),
+                               Lattice(ix+1, iy, fx1, fy0),
+                               wx);
+		float vy1 = Mathf.Lerp(Lattice(ix, iy+1, fx0, fy1),
+                               Lattice(ix+1, iy+1, fx1, fy1),
+                               wx);
+		return Mathf.Lerp(vy0, vy1, wy);
+	}
+	
+	public float Noise(Vector3 v)
+	{
+		int ix = Mathf.FloorToInt(v.x);
+		float fx0 = v.x - ix;
+		float fx1 = fx0- 1f;
+		float wx = Smooth(fx0);
+		
+		int iy = Mathf.FloorToInt(v.y);
+		float fy0 = v.y - iy;
+		float fy1 = fy0 - 1f;
+		float wy = Smooth(fy0);
+		
+		int iz = Mathf.FloorToInt(v.z);
+		float fz0 = v.z - iz;
+		float fz1 = fz0 - 1f;
+		float wz = Smooth(fz0);
+		
+		float vy0 = Mathf.Lerp(Lattice(ix, iy, iz, fx0, fy0, fz0),
+		                       Lattice(ix+1, iy, iz, fx1, fy0, fz0),
+		                       wx);
+		float vy1 = Mathf.Lerp(Lattice(ix, iy+1, iz, fx0, fy1, fz0),
+		                       Lattice(ix+1, iy+1, iz, fx1, fy1, fz0),
+		                       wx);
+		
+		float vz0 = Mathf.Lerp(vy0, vy1, wy);
+		
+		vy0 = Mathf.Lerp(Lattice(ix, iy, iz+1, fx0, fy0, fz1),
+		                 Lattice(ix+1, iy, iz+1, fx1, fy0, fz1),
+		                 wx);
+		vy1 = Mathf.Lerp(Lattice(ix, iy+1, iz+1, fx0, fy1, fz1),
+		                 Lattice(ix+1, iy+1, iz+1, fx1, fy1, fz1),
+		                 wx);
+		float vz1 = Mathf.Lerp(vy0, vy1, wy);
+		
+		return Mathf.Lerp(vz0, vz1, wz);
+	}
+
+	private float Lattice(int ix, int iy, int iz, float fx, float fy, float fz)
+	{
+		int g = FPermutations[(ix+FPermutations[(iy+FPermutations[iz & cMask]) & cMask]) & cMask]*3;
+		return FGradients[g]*fx + FGradients[g+1]*fy + FGradients[g+2]*fz;
+	}
+	
+	private float Lattice(int ix, int iy, float fx, float fy)
+	{
+		int g = FPermutations[(ix+FPermutations[(iy+FPermutations[0]) & cMask]) & cMask]*3;
+		return FGradients[g]*fx + FGradients[g+1]*fy;
+	}
+	
+	private float Smooth(float x)
+	{
+		return x*x*(3f-2f*x);
+	}
+}
