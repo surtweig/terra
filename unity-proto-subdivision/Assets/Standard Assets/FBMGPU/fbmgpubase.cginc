68ffeea10a668850167d62f9fe0f1190689adb8e
surtweig <surtweig@gmail.com>
2014-05-03 18:32:40 +0400

    added: FladerakDiffuse shader; fbmgpubase, fbmgpulib; Geosphere

---
+++ unity-proto-subdivision/Assets/Standard Assets/FBMGPU/fbmgpubase.cginc
@@ -0,0 +1,178 @@
+#define THREADGROUPSIZE 64
+#define SPECTRUM_MAX_LENGTH 16
+
+//#define RNDMETHOD_SIMPLE
+#define RNDMETHOD_POHORESKI
+
+#define KERNEL(func)                [numthreads(THREADGROUPSIZE,1,1)] void k_##func (uint3 id : SV_DispatchThreadID) { values[id.x].val = func(points[id.x].pos, baseOctaves, baseSpectrum); } 
+#define KERNEL_P1(func, p1)         [numthreads(THREADGROUPSIZE,1,1)] void k_##func (uint3 id : SV_DispatchThreadID) { values[id.x].val = func(points[id.x].pos, baseOctaves, baseSpectrum, p1); } 
+#define KERNEL_P2(func, p1, p2)     [numthreads(THREADGROUPSIZE,1,1)] void k_##func (uint3 id : SV_DispatchThreadID) { values[id.x].val = func(points[id.x].pos, baseOctaves, baseSpectrum, p1, p2); } 
+#define KERNEL_P3(func, p1, p2, p3) [numthreads(THREADGROUPSIZE,1,1)] void k_##func (uint3 id : SV_DispatchThreadID) { values[id.x].val = func(points[id.x].pos, baseOctaves, baseSpectrum, p1, p2, p3); } 
+
+typedef RWStructuredBuffer<float> SPECTRUM;
+
+struct PointInput
+{
+	float3 pos;
+};
+
+struct PerlinOutput
+{
+	float val;
+};
+
+struct SpectrumStruct
+{
+	float values[SPECTRUM_MAX_LENGTH];
+	int octaves;
+};
+
+RWStructuredBuffer<PointInput> points;
+RWStructuredBuffer<PerlinOutput> values;
+
+int baseOctaves;
+float gamma;
+SPECTRUM baseSpectrum;
+
+#ifdef RNDMETHOD_SIMPLE
+float rand (float3 pos)
+{
+      return 0.5+(frac(sin(dot(pos, float3(12.9898, 78.233, -47.2937))) * 43758.5453))*0.5;
+}
+#endif
+
+#ifdef RNDMETHOD_POHORESKI
+// Input: It uses texture coords as the random number seed.
+// Output: Random number: [0,1), that is between 0.0 and 0.999999... inclusive.
+// Author: Michael Pohoreski
+// Copyright: Copyleft 2012 :-)
+float rand (float3 pos)
+{
+  // We need irrationals for pseudo randomness.
+  // Most (all?) known transcendental numbers will (generally) work.
+  const float3 r = float3(
+    23.1406926327792690, // e^pi (Gelfond's constant)
+    2.6651441426902251,  // 2^sqrt(2) (Gelfond–Schneider constant)
+	3.1415926535897932); 
+  return frac( cos( fmod( 123456789.0, 1e-7 + 256.0 * dot(pos, r) ) ) );  
+}
+#endif
+
+float3 rand3 (float3 pos)
+{
+	return float3( rand(pos.xyz), rand(pos.yzx), rand(pos.zxy) );
+}
+
+static float3 rndvector = float3(28.9017903, 53.7600885, 49.0959582);
+
+static uint permutation4[128] = {
+1535746199, 226692954, 895508425, 3775392194, 510076044, 1661505093, 169209893, 2483469847,
+1273657591, 1053104640, 3420191838, 537600885, 1478603065, 1463326189, 2289898670, 2940512427,
+2252842314, 2786799755, 3885929037, 2061856595, 3867530044, 693922268, 687156791, 915367668,
+2705267009, 1230034945, 3146009809, 2836552144, 2189935816, 1453309044, 3329057956, 1073986221,
+4209170740, 3389356924, 2121700134, 3562361599, 3812347599, 289017903, 706526646, 3584735199,
+43579511, 1185126956, 2607127005, 162278311, 4247197313, 1852596755, 3907023183, 1752218034,
+3831625434, 3253871355, 210817774, 4053971903, 3952161617, 1810829049, 534167601, 2641020853,
+2966181048, 758282611, 4271244415, 1573776522, 490959582, 2381531160, 1112458112, 3030138327,
+
+1535746199, 226692954, 895508425, 3775392194, 510076044, 1661505093, 169209893, 2483469847,
+1273657591, 1053104640, 3420191838, 537600885, 1478603065, 1463326189, 2289898670, 2940512427,
+2252842314, 2786799755, 3885929037, 2061856595, 3867530044, 693922268, 687156791, 915367668,
+2705267009, 1230034945, 3146009809, 2836552144, 2189935816, 1453309044, 3329057956, 1073986221,
+4209170740, 3389356924, 2121700134, 3562361599, 3812347599, 289017903, 706526646, 3584735199,
+43579511, 1185126956, 2607127005, 162278311, 4247197313, 1852596755, 3907023183, 1752218034,
+3831625434, 3253871355, 210817774, 4053971903, 3952161617, 1810829049, 534167601, 2641020853,
+2966181048, 758282611, 4271244415, 1573776522, 490959582, 2381531160, 1112458112, 3030138327
+};
+
+	
+float fade (float t)
+{
+	return t * t * t * (t * (t * 6.0 - 15.0) + 10.0);
+}
+	
+float grad (int hash, float x, float y, float z)
+{
+	int h = hash & 15;
+	float u = h<8 ? x : y;
+	float v = h<4 ? y : h==12||h==14 ? x : z;
+	return ((h&1) == 0 ? u : -u) + ((h&2) == 0 ? v : -v);
+}
+
+uint p4i (uint index)
+{
+	uint u = index / 4;
+	uint b = index % 4;
+	return (permutation4[u] & (255 << b*8)) >> b*8;
+}
+	
+float perlin (float3 p)
+{
+	uint3 i = uint3( int(floor(p.x)) & 255, int(floor(p.y)) & 255, int(floor(p.z)) & 255 );
+
+	float3 f = p-floor(p);
+	
+	float u = fade(f.x);
+	float v = fade(f.y);
+	float w = fade(f.z);
+	
+	uint A  = (p4i(i.x) + i.y);
+	uint AA = (p4i(A) + i.z);
+	uint AB = (p4i(A+1) + i.z);
+	uint B  = (p4i( i.x+1 ) + i.y);
+	uint BA = (p4i(B) + i.z);
+	uint BB = (p4i( B+1 ) + i.z);
+	
+	return lerp(lerp(lerp(grad(p4i(AA), f.x  , f.y  , f.z   ),  
+                          grad(p4i(BA), f.x-1.0, f.y  , f.z   ),
+						  u), 
+                     lerp(grad(p4i(AB), f.x  , f.y-1.0, f.z   ),  
+                          grad(p4i(BB), f.x-1.0, f.y-1.0, f.z   ),
+						  u),
+					 v),
+                lerp(lerp(grad(p4i(AA+1), f.x  , f.y  , f.z-1.0 ),  
+                          grad(p4i(BA+1), f.x-1.0, f.y  , f.z-1.0 ),
+						  u), 
+                     lerp(grad(p4i(AB+1), f.x  , f.y-1.0, f.z-1.0 ),
+                          grad(p4i(BB+1), f.x-1.0, f.y-1.0, f.z-1.0 ),
+						  u),
+				     v),
+				w);	
+}
+
+float FBM (float3 p, int octaves, SPECTRUM spectrum)
+{
+	float v = 0.0;
+	float3 t = p;
+	
+	for (int octave = 0; octave < octaves; octave++)
+	{
+		v += perlin(t)*spectrum[octave];
+		t *= 2.0;
+	}
+	
+	/*v = 0.0;
+	int i;
+	if (p.y < 0.1)
+	{	
+		i = (int)(p.x);
+		if (i < 0) i = 0;
+		if (i >= 16) i = 15;
+		v = pow(2.0, -i*0.5);
+	}
+	if (p.y >= 0.2 && p.y < 0.3)
+	{	
+		i = (int)(p.x);
+		if (i < 0) i = 0;
+		if (i >= 16) i = 15;
+		v = spectrum[i];
+	}*/
+
+	return v;
+}
+
+
+float3 FBMVector (float3 p, int octaves, SPECTRUM spectrum)
+{
+	return float3(FBM(p, octaves, spectrum), FBM(p+rndvector, octaves, spectrum), FBM(p+2.0*rndvector, octaves, spectrum));
+}
---
+++ unity-proto-subdivision/Assets/Standard Assets/Geosphere/Geosphere.cs
@@ -0,0 +1,332 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+
+
+public class GeoNode
+{
+	public Vector3 position;
+	public Vector3 normal;
+	public float radius;
+	public List<int[]> adjacency;
+	public bool transformed = false;
+	
+	public GeoNode()
+	{
+		adjacency = new List<int[]>();
+	}
+}
+
+
+public class GeoTriTreeNode
+{
+	public int[] vertices;
+	public int[] children;
+}
+
+
+public class MutableGeoMesh
+{
+	public List<int> indexes;
+	public List<int> triangles;
+	
+	public MutableGeoMesh()
+	{
+		indexes = new List<int>();
+		triangles = new List<int>();
+	}
+	
+	public void AddVertex(int nodeIndex)
+	{
+		if (indexes.Count == 0)
+			indexes.Add(nodeIndex);
+		else
+		{
+			int subIndex = indexes.BinarySearch(nodeIndex);
+			if (subIndex < 0)
+			{
+				subIndex = ~subIndex;
+				indexes.Insert(subIndex, nodeIndex);
+			}
+			
+		}
+		triangles.Add(nodeIndex);
+	}
+	
+	public void IndexTriangles()
+	{
+		for (int i = 0; i < triangles.Count; i++)
+			triangles[i] = indexes.BinarySearch(triangles[i]);
+	}
+}
+
+
+public class MeshContainer
+{
+	public Vector3[] vertices;
+	public Vector3[] normals;
+	public Vector2[] uv;
+	public int[] triangles;
+	
+	public Mesh MakeMesh()
+	{
+		Mesh mesh = new Mesh();
+		mesh.vertices = vertices;
+		mesh.normals = normals;
+		mesh.triangles = triangles;
+		mesh.uv = uv;
+		return mesh;
+	}
+}
+
+
+public class DummyNoiseGenerator : ISpatialNoiseGenerator
+{
+	private int pointsNumber;
+	
+	public void Start(Vector3[] points, int steps)
+	{
+		pointsNumber = points.Length;
+	}
+	
+	public bool Update()
+	{
+		return true;
+	}
+	
+	public float[] Output()
+	{
+		float[] output = new float[pointsNumber];
+		for (int i = 0; i < output.Length; i++)
+			output[i] = 0f;
+		return output;
+	}
+}
+
+public interface IGeoSurfaceConsumer
+{
+	void OnMeshReady(GeoSurface sender, MeshContainer meshContainer);
+}
+
+public class GeoSurface
+{
+	protected List<GeoNode> nodes;
+	protected List<GeoTriTreeNode> tritree;
+	protected int xSubdivisionLevel;
+	
+	public IGeoSurfaceConsumer Consumer = null;
+	public ISpatialNoiseGenerator Noise;
+	
+	public GeoSurface()
+	{
+		nodes = new List<GeoNode>();
+		tritree = new List<GeoTriTreeNode>();
+		Noise = new DummyNoiseGenerator();
+	}
+	
+	public virtual Vector3 NormalizePoint(Vector3 point)
+	{
+		return point;
+	}
+	
+	public virtual Vector3 ElevatePoint(Vector3 point, float elevation)
+	{
+		return point;
+	}
+	
+	public void BuildNormals()
+	{
+		for (int i = 0; i < nodes.Count; i++)
+		{
+			float nx = 0f;
+			float ny = 0f;
+			float nz = 0f;
+			
+			int highlev = nodes[i].adjacency.Count-1;
+			
+			int icurrent = -1;
+			Vector3 vcurrent = nodes[i].position;
+
+			int inext = nodes[i].adjacency[highlev][0];
+			Vector3 vnext = nodes[inext].position;
+			
+			int ifirst = inext;
+			
+			while (true)
+			{
+				nx += (vcurrent.y - vnext.y) * (vcurrent.z + vnext.z);
+				ny += (vcurrent.z - vnext.z) * (vcurrent.x + vnext.x);
+				nz += (vcurrent.x - vnext.x) * (vcurrent.y + vnext.y);
+				
+				// finding next node as common adjacent of center and current node, but not previous
+				bool nextfound = false;
+				for (int j = 0; j < nodes[i].adjacency[highlev].Length; j++)
+				{
+					for (int k = 0; k < nodes[inext].adjacency[highlev].Length; k++)
+						if (nodes[i].adjacency[highlev][j] == nodes[inext].adjacency[highlev][k])
+							if (nodes[i].adjacency[highlev][j] != icurrent)
+							{
+								icurrent = inext;
+								inext = nodes[i].adjacency[highlev][j];
+								nextfound = true;
+								break;
+							}
+					
+					if (nextfound)
+						break;
+				}
+				
+				vcurrent = vnext;
+				vnext = nodes[inext].position;
+				
+				if (inext == ifirst)
+					break;
+			}
+			
+			vnext = nodes[i].position;
+			nx += (vcurrent.y - vnext.y) * (vcurrent.z + vnext.z);
+			ny += (vcurrent.z - vnext.z) * (vcurrent.x + vnext.x);
+			nz += (vcurrent.x - vnext.x) * (vcurrent.y + vnext.y);
+			
+			Vector3 normal = new Vector3(nx, ny, nz).normalized;
+			
+			if (Vector3.Dot(normal, vnext.normalized) < 0)
+				normal = -normal;
+			
+			nodes[i].normal = normal;
+		}
+	}
+	
+	private int xAddNode(GeoNode node)
+	{
+		int i = nodes.Count;
+		nodes.Add(node);
+		return i;
+	}
+	
+	private int xAddTriTreeNode(int[] vertices, int parentNode = -1)
+	{
+		GeoTriTreeNode newNode = new GeoTriTreeNode();
+		newNode.vertices = vertices;
+		newNode.children = new int[4] { -1, -1, -1, -1 };
+		int newNodeIndex = tritree.Count;
+		tritree.Add(newNode);
+		if (parentNode >= 0)
+			for (int i = 0; i < 4; i++)
+				if (tritree[parentNode].children[i] == -1)
+				{
+					tritree[parentNode].children[i] = newNodeIndex;
+					break;
+				}
+		return newNodeIndex;
+	}
+	
+	private void xAddNodeAdjacency(int nodeIndex, int level, int adjNodeIndex)
+	{
+		int maxLevel = nodes[nodeIndex].adjacency.Count-1;
+		if (maxLevel < level)
+			for (int i = maxLevel; i < level; i++)
+				nodes[nodeIndex].adjacency.Add(new int[] {});
+		
+		int[] adj = new int[nodes[nodeIndex].adjacency[level].Length+1];
+		nodes[nodeIndex].adjacency[level].CopyTo(adj, 0);
+		adj[adj.Length-1] = adjNodeIndex;
+		nodes[nodeIndex].adjacency[level] = adj;
+	}
+	
+	private void xConnectNodes(int node1, int node2, int level)
+	{
+		xAddNodeAdjacency(node1, level, node2);
+		xAddNodeAdjacency(node2, level, node1);
+	}
+	
+	private int xFindNodesCommonAdjacency(int node1, int node2, int level)
+	{
+		if (nodes[node1].adjacency.Count <= level || nodes[node2].adjacency.Count <= level)
+			return -1;
+		foreach (int adj1 in nodes[node1].adjacency[level])
+			foreach (int adj2 in nodes[node2].adjacency[level])
+				if (adj1 == adj2)
+					return adj1;
+		return -1;
+	}	
+	
+	protected void xCollectVertexes(MutableGeoMesh mesh, int baseTriangleNode, int depth = -1)
+	{
+		if (depth == 0 || tritree[baseTriangleNode].children[0] < 0)
+		{
+			for (int i = 0; i < 3; i++)
+				mesh.AddVertex(tritree[baseTriangleNode].vertices[i]);
+		}
+		else
+		{
+			for (int i = 0; i < 4; i++)
+				xCollectVertexes(mesh, tritree[baseTriangleNode].children[i], depth-1);
+		}
+	}
+	
+	protected void xSubdivide(int treeNodeIndex)
+	{
+		if (tritree[treeNodeIndex].children[0] < 0)
+		{
+			int[] subNodes = new int[3];
+			for (int i = 0; i < 3; i++)
+			{
+				int v1 = tritree[treeNodeIndex].vertices[i];
+				int v2 = tritree[treeNodeIndex].vertices[(i+1) % 3];
+				
+				int v12 = xFindNodesCommonAdjacency(v1, v2, xSubdivisionLevel+1);
+				
+				if (v12 < 0)
+				{
+					GeoNode newnode = new GeoNode();
+					newnode.position = (nodes[v1].position + nodes[v2].position).normalized;
+					newnode.radius = 1f;
+					newnode.adjacency = new List<int[]>();
+					v12 = xAddNode(newnode);
+					
+					xConnectNodes(v12, v1, xSubdivisionLevel+1);
+					xConnectNodes(v12, v2, xSubdivisionLevel+1);
+				}
+				
+				subNodes[i] = v12;
+			}
+			
+			xConnectNodes(subNodes[0], subNodes[1], xSubdivisionLevel+1);
+			xConnectNodes(subNodes[1], subNodes[2], xSubdivisionLevel+1);
+			xConnectNodes(subNodes[2], subNodes[0], xSubdivisionLevel+1);
+			
+			//      /\
+			//     /  \
+			//    /----\
+			//   / \  / \
+			//  /___\/___\
+			
+			xAddTriTreeNode( new int[3] { tritree[treeNodeIndex].vertices[0], subNodes[0], subNodes[2] }, treeNodeIndex );
+			xAddTriTreeNode( new int[3] { tritree[treeNodeIndex].vertices[1], subNodes[1], subNodes[0] }, treeNodeIndex );
+			xAddTriTreeNode( new int[3] { tritree[treeNodeIndex].vertices[2], subNodes[2], subNodes[1] }, treeNodeIndex );
+			xAddTriTreeNode( new int[3] { subNodes[0], subNodes[1], subNodes[2] }, treeNodeIndex );
+		}
+		else
+			foreach (int childNode in tritree[treeNodeIndex].children)
+				xSubdivide(childNode);
+	}
+}
+
+
+public class GeoQuad : GeoSurface
+{
+	
+	//     C1__________B
+	//      /\        /
+	//     /  \      /
+	//    /    \    /
+	//   /      \  /
+	//  /________\/
+	//  A         C2
+	
+	public GeoQuad(Vector3 pA, Vector3 pB, Vector3 pC1, Vector3 pC2) : base()
+	{
+	}
+}
